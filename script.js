e44adac4d134cb5e5e9f3528910d7dadfa2af58a
Bhoomika P M <bhoomikapm.0116@gmail.com>
2024-12-01 21:00:56 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 let startBtn = document.getElementById('start');
 let stopBtn = document.getElementById('stop');
 let resetBtn = document.getElementById('reset');
+let shareBtn = document.getElementById('share');
+let setIntervalBtn = document.getElementById('setIntervalBtn');
+let intervalInput = document.getElementById('intervalInput');
+let challengeStatus = document.getElementById('challenge-status');
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+let hour = 0;
+let minute = 0;
+let second = 0;
+let count = 0;
+let timer = false;
+let interval = null;
+let challengeStarted = false;
+let challengeTarget = 10; 
+
+let intervalTime = null; 
+let intervalTimeout = null;
 
 startBtn.addEventListener('click', function () {
     timer = true;
@@ -14,6 +25,7 @@ startBtn.addEventListener('click', function () {
 
 stopBtn.addEventListener('click', function () {
     timer = false;
+    checkChallenge();
 });
 
 resetBtn.addEventListener('click', function () {
@@ -26,6 +38,39 @@ resetBtn.addEventListener('click', function () {
     document.getElementById('min').innerHTML = "00";
     document.getElementById('sec').innerHTML = "00";
     document.getElementById('count').innerHTML = "00";
+    challengeStatus.innerHTML = "Current Challenge: Not Started";
+    clearInterval(intervalTimeout); 
+});
+
+setIntervalBtn.addEventListener('click', function () {
+    intervalTime = parseInt(intervalInput.value);
+    if (intervalTime < 1) {
+        alert("Interval must be 1 second or more!");
+        return;
+    }
+    clearInterval(intervalTimeout);
+    intervalTimeout = setInterval(function () {
+        alert(`Interval reached! ${intervalTime} seconds passed.`);
+    }, intervalTime * 1000);
+});
+
+shareBtn.addEventListener('click', function () {
+    let currentTime = `${document.getElementById('hr').innerHTML}:${document.getElementById('min').innerHTML}:${document.getElementById('sec').innerHTML}:${document.getElementById('count').innerHTML}`;
+    let shareMessage = `Check out my stopwatch: ${currentTime}`;
+    
+    if (navigator.share) {
+        navigator.share({
+            title: 'My Stopwatch',
+            text: shareMessage,
+            url: window.location.href
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        }).catch((error) => {
+            console.log('Error sharing: ', error);
+        });
+    } else {
+        alert("Share API is not supported on this browser.");
+    }
 });
 
 function stopWatch() {
@@ -73,6 +118,21 @@ function stopWatch() {
         document.getElementById('min').innerHTML = minString;
         document.getElementById('sec').innerHTML = secString;
         document.getElementById('count').innerHTML = countString;
+        
         setTimeout(stopWatch, 10);
+    }
+}
+
+function checkChallenge() {
+    if (!challengeStarted) {
+        challengeStarted = true;
+        challengeStatus.innerHTML = `Current Challenge: Try to stop at ${challengeTarget} seconds!`;
+    }
+
+    let currentTime = hour * 3600 + minute * 60 + second + count / 100;
+    if (Math.abs(currentTime - challengeTarget) < 0.1) {
+        challengeStatus.innerHTML = `You did it! Challenge passed at ${currentTime.toFixed(2)} seconds.`;
+    } else {
+        challengeStatus.innerHTML = `Challenge failed! You stopped at ${currentTime.toFixed(2)} seconds.`;
     }
 }
